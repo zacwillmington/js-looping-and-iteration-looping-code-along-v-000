2f5e15136c14df076de0aa4632cebc55597f51cc
Zac Willmington <zac.willmington@gmail.com>
2018-09-08 01:33:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function printBadges(names) {
+    for(i = 0; i < names.length; i++){
+        console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+    }
+    return names;
+}
+
+
+function maybeTrue() {
+    return Math.random() >= 0.5;
+}
+
+function tailsNeverFails() {
+    let counter = 0;
+    while(!maybeTrue()){
+        counter++
+    }
+    console.log(`You got ${counter} Tails in a row!`);
+}
